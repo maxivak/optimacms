bb98af9c1e8e0d18a796708ea5337ad0981bddee
Max Ivak <maxivak@gmail.com>
2017-04-22 20:15:07 +0300

    edit content from site

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,6 @@
+Rails.application.config.assets.precompile += %w( bootstrap4/bootstrap.css )
+Rails.application.config.assets.precompile += %w( optimacms/grid_common.css )
+
 Rails.application.config.assets.precompile += %w( tinymce-jquery.js )
 Rails.application.config.assets.precompile += %w( optimacms/tinymce.css )
 Rails.application.config.assets.precompile += %w(tinymce/plugins/advimage/plugin.js tinymce/plugins/uploadimage/plugin.js tinymce/plugins/uploadimage/langs/en.js )
