9b83928055bac49ccb0d2ee49facecd7751926a8
mmx <maxivak@gmail.com>
2017-09-24 22:12:13 +0300

    backup config

--- spec/dummy/config/initializers/optimacms.rb
+++ spec/dummy/config/initializers/optimacms.rb
@@ -2,3 +2,7 @@ Optimacms.main_namespace = '' # blank or any like 'cms'
 Optimacms.admin_namespace = 'admin'
 
 Optimacms.files_dir_path = 'img'
+
+Optimacms.backup_metadata = {
+    dir_base: "/data/backups/cms/metadata"
+}
