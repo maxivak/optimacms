a1e22c7b2fb87efc920416e1b70803fc95fb5a7a
mmx <maxivak@gmail.com>
2018-03-09 23:40:37 +0200

    demo app, css

--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -6,9 +6,12 @@ Rails.application.config.assets.version = '1.0'
 # yarn
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+Rails.application.config.assets.precompile << %r{font-awesome/fonts/[\w-]+\.(?:eot|svg|ttf|woff2?)$}
+
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( tpl.css )
+#Rails.application.config.assets.precompile += %w( tpl.css )
