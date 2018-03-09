e93cc42e7955ce4a9f6395b1a7cff4080cf8b822
mmx <maxivak@gmail.com>
2018-03-09 23:38:50 +0200

    fonts, css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,9 +1,11 @@
 # yarn
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+Rails.application.config.assets.precompile << %r{font-awesome/fonts/[\w-]+\.(?:eot|svg|ttf|woff2?)$}
+
 
 #
-Rails.application.config.assets.precompile += %w( bootstrap4/bootstrap.css )
+#Rails.application.config.assets.precompile += %w( bootstrap4/bootstrap.css )
 
 Rails.application.config.assets.precompile += %w( tinymce-jquery.js )
 Rails.application.config.assets.precompile += %w( optimacms/tinymce.css )
