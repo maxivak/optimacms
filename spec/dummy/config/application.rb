fede28fc45dadc50b79752c438c0ba5a85f2f599
mmx <maxivak@gmail.com>
2018-12-14 12:49:44 +0200

    rails 5.2

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -16,7 +16,7 @@ require "optimacms"
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
 
     # Settings in config/environments/* take precedence over those specified here.
