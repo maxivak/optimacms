5c521ab9cede7b3299957d876b880f0ac9957bb8
mmx <maxivak@gmail.com>
2020-09-06 21:33:18 -0700

    fix temp Local class

--- lib/optimacms/remote_content/temp_local.rb
+++ lib/optimacms/remote_content/temp_local.rb
@@ -1,57 +1,69 @@
 module Optimacms
-  module ContentBlock
-    class Local < Base
-
-
-      def init_local_file
-        local_file_basepath = nil
-
-        name = path
-        lang = options[:lang]
-
-
-        # find template by path
-
-        template = Optimacms::PageServices::TemplateService.get_by_name(path)
-
-        if template
-          if template.is_translated?
-            local_file_basepath = template.path(lang)
-          else
-            local_file_basepath = template.path(nil)
-          end
-
-
-        else
-          # find file on disk
-          f_opts = {}
-          f_opts[:lang] = lang
-
-          page_template_path = options[:page_template_path]
-          if page_template_path
-            d = File.dirname(options[:page_template_path]) # OK 2019-jan
-          else
-            d=''
-          end
-
-          f_opts[:current_dir] = d
-
-          local_file_basepath = PageServices::TemplateService.get_file_for_path(path, f_opts)
-
-        end
-
-
-        # set
-        if local_file_basepath
-          local_file.update_basepath local_file_basepath
-        end
-
-
-        true
-      end
-
+  module RemoteContent
+    class TempLocal
 
     end
   end
+
 end
 
+
+
+
+# module Optimacms
+#   module ContentBlock
+#     class Local < Base
+#
+#
+#       def init_local_file
+#         local_file_basepath = nil
+#
+#         name = path
+#         lang = options[:lang]
+#
+#
+#         # find template by path
+#
+#         template = Optimacms::PageServices::TemplateService.get_by_name(path)
+#
+#         if template
+#           if template.is_translated?
+#             local_file_basepath = template.path(lang)
+#           else
+#             local_file_basepath = template.path(nil)
+#           end
+#
+#
+#         else
+#           # find file on disk
+#           f_opts = {}
+#           f_opts[:lang] = lang
+#
+#           page_template_path = options[:page_template_path]
+#           if page_template_path
+#             d = File.dirname(options[:page_template_path]) # OK 2019-jan
+#           else
+#             d=''
+#           end
+#
+#           f_opts[:current_dir] = d
+#
+#           local_file_basepath = PageServices::TemplateService.get_file_for_path(path, f_opts)
+#
+#         end
+#
+#
+#         # set
+#         if local_file_basepath
+#           local_file.update_basepath local_file_basepath
+#         end
+#
+#
+#         true
+#       end
+#
+#
+#     end
+#   end
+# end
+#
